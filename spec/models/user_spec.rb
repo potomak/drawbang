637070e6510dc4ce34fc70dc1bb67a1ad1af27e2
Giovanni <potomak84@gmail.com>
2011-06-16 17:22:15 +0200

    add before :each block to model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,21 @@
 require 'spec/spec_helper'
 
 describe User do
+  before :each do
+    @key = "user:123"
+    @user = {:name => "john"}
+  end
+  
   it "should save user" do
-    key = "user:123"
-    user = {:name => "john"}
+    REDIS.should_receive(:set).with(@key, @user.to_json)
     
-    REDIS.should_receive(:set).with(key, user.to_json)
-    
-    User.new(user.merge(:key => key)).save
+    User.new(@user.merge(:key => @key)).save
   end
   
   it "should find user" do
-    key = "user:123"
-    user = {:name => "john"}
+    REDIS.should_receive(:get).with(@key).and_return(@user.to_json)
+    JSON.should_receive(:parse).with(@user.to_json).and_return(@user)
     
-    REDIS.should_receive(:get).with(key).and_return(user.to_json)
-    JSON.should_receive(:parse).with(user.to_json).and_return(user)
-    
-    User.find(key).should == user
+    User.find(@key).should == @user
   end
 end
