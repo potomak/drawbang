0f8fe20630c2b03cff1a94fbb03482f1e01129e6
Giovanni Cappellotto <potomak84@gmail.com>
2012-01-02 18:27:22 +0100

    Add profile migration
    Closes #9

--- db/migrate/003_create_profile_lists.rb
+++ db/migrate/003_create_profile_lists.rb
@@ -1,1 +1,13 @@
-# TODO
+require 'server'
+
+drawings = Drawing.all :per_page => 999999
+puts "#{drawings.size} drawings found"
+
+drawings.each do |drawing|
+  puts "drawing id #{drawing[:id]}"
+  
+  if drawing['user'] && drawing['user']['uid']
+    puts "push drawing to #{Drawing.list(drawing['user']['uid'])}"
+    REDIS.rpush(Drawing.list(drawing['user']['uid']), drawing['id'])
+  end
+end
