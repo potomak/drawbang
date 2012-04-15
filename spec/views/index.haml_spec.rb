75f8ae6125691e716f1d56124281054a6eceb07d
Giovanni Cappellotto <potomak84@gmail.com>
2012-04-15 02:57:23 +0200

    Fix index spec

--- spec/views/index.haml_spec.rb
+++ spec/views/index.haml_spec.rb
@@ -2,7 +2,14 @@ require 'spec/spec_helper'
 
 describe "index.haml" do
   it "should display 'Gallery' title" do
-    render("/views/index.haml")
+    render("/views/index.haml", :params => {})
     rendered.should match(/Gallery/)
+  end
+
+  it "should include javascript to remove app requests" do
+    render("/views/index.haml", :params => {:request_ids => "1,2,3"})
+    rendered.should match(/FB\.api\('1/)
+    rendered.should match(/FB\.api\('2/)
+    rendered.should match(/FB\.api\('3/)
   end
 end
